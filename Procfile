d4ec90cda9c53ef6f7058214c62579d88b065d76
Vlad_ivn <vladislavivaniuk@gmail.com>
2024-09-14 13:14:25 +0300

    Fix binding to PORT for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python server.py
+web: hypercorn server:app --bind 0.0.0.0:$PORT
--- server.py
+++ server.py
@@ -87,8 +87,8 @@ async def success_page():
     return "Вы успешно проголосовали!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
